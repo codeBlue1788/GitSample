4e567f45f4d02714d37d378ba20e99db1574b12e
Ryan Lee <ctlee1788@gmail.com>
2021-02-01 15:56:12 +0800

    2nd Version

---
+++ JDBC/src/testApp/test.java
@@ -0,0 +1,15 @@
+package testApp;
+
+public class test {
+
+	public static void main(String[] args) {
+		for(int i = 1 ; i<=9; i ++) {
+			for(int j = 1 ;j<=9;j++) {
+				System.out.print(i + "X" + j +"=" + i*j+"\t");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
